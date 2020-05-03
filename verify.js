dc387d5a3946d3f20b592aa6a0095acc637e343f
cchromak <cchromak@gmail.com>
2020-05-02 22:20:17 -0400

    started general css

---
+++ verify.js
@@ -0,0 +1,44 @@
+function ValidateEmail() 
+{
+    var email = document.getElementById('email').value
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+  {
+    return (true)
+    
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+
+function CheckPasswordStudent() 
+{ 
+    var pword = document.getElementById('password').value
+    var email = document.getElementById('email').value
+    
+    location.href='student.html';
+    
+// if(pword.match(passw)) 
+// { 
+// return true;
+// }
+// else
+// { 
+// alert('Password can only contain letters, max size 10')
+// return false;
+// }
+}
+
+function CheckPasswordProf() 
+{ 
+    var pword = document.getElementById('password').value
+    var email = document.getElementById('email').value
+    
+    if(pword == 'abc'){
+
+    location.href='professor.html';
+    }else{
+        alert('password is dead wrong bro')
+        return false
+    }
+}
