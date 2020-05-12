cc033f57ee55c34be49fab13310bddf9dbf38740
cchromak <cchromak@gmail.com>
2020-05-12 17:37:53 -0400

    fixed verfy email

--- registration.php
+++ registration.php
@@ -9,10 +9,16 @@ $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+$login = $_POST['login'];
+$pwd = $_POST['password'];
+
 echo "Connected successfully";
+
+mysqli_close($db);
+
 ?>
 
-<!-- 
+
 
 <?php
 
@@ -31,8 +37,8 @@ if (isset($_POST['login'])) {
         array_push($errors, "Password is required");
     }
     if (count($errors) == 0) {
-        $password = md5($password);
-        $query = "Select * from users where UserEmail = '$Login' AND UserPassword = '$password' ";
+      
+        $query = "Select * from appuser where login = '$login' AND pwd = '$password' ";
         $results = mysqli_query($db, $query);
 
         if (mysqli_num_rows($results)) {
@@ -44,74 +50,87 @@ if (isset($_POST['login'])) {
             array_push($errors, "Wrong Username/Password combination. Please try again.");
         }
     }
+mysqli_close($db);
+ }
+
+ else
+
+ {
+session_start() ;
+
+
+$errors = array() ;
+
+//register users
+
+$name = $_POST['name'] ;
+$confirmpassword = $_POST['confirmpassword'] ;
+
+//form validation
+
+if( empty($name) )
+{
+    array_push( $errors , "Name is required") ;
+} 
+
+if( empty($email) )
+{
+    array_push($errors , "email is required") ;
+} 
+
+if( empty($password))
+{
+    array_push($errors , "Password is required") ;
+} 
+
+if( $password != $confirmpassword )
+{
+    array_push($errors , "Password do not match" ) ;
+}
+
+//check database for existing user with same username
+$user_check_query = "Select * from appuser where UserEmail = '$email' LIMIT 1" ;
+
+$results = mysqli_query( $db , $user_check_query ) ;
+$user = mysqli_fetch_assoc($results) ;
+
+
+if($user)
+{
+    if($user["UserEmail"] === $email)
+    {
+        array_push($errors , "This email is already registered" ) ;
+    }
+}
+
+//Register user if no errors
+
+if( count($errors) == 0 )
+{
+    $password = md5($password) ; //This will encrypt password
+
+    $query = "Insert into appuser (UserName , UserEmail , UserPassword ) values ( '$name' , '$email' , '$password' )" ;
+    
+    mysqli_query($db , $query ) ;
+
+    $_SESSION['UserName'] = $name ;
+    $_SESSION['success'] = "You are now signed Up" ;
+
+    header( 'location: index.php' ) ;
     mysqli_close($db);
-} else {
-    session_start();
+}
+ 
+}
 
-
-    $errors = array();
-
-    //register users
-
-    $name = $_POST['name'];
-    $confirmpassword = $_POST['confirmpassword'];
-
-    //form validation
-
-    if (empty($name)) {
-        array_push($errors, "Name is required");
-    }
-
-    if (empty($email)) {
-        array_push($errors, "email is required");
-    }
-
-    if (empty($password)) {
-        array_push($errors, "Password is required");
-    }
-
-    if ($password != $confirmpassword) {
-        array_push($errors, "Password do not match");
-    }
-
-    //check database for existing user with same username
-    $user_check_query = "Select * from users where UserEmail = '$email' LIMIT 1";
-
-    $results = mysqli_query($db, $user_check_query);
-    $user = mysqli_fetch_assoc($results);
-
-
-    if ($user) {
-        if ($user["email"] === $email) {
-            array_push($errors, "This email is already registered");
-        }
-    }
-
-    //Register user if no errors
-
-    if (count($errors) == 0) {
-        $password = md5($password); //This will encrypt password
-
-        $sql = "INSERT INTO appuser (login, pwd, first_name, last_name, email, user_type) VALUES('$userName', '$password', '$first', '$last', '$email', '$user_type')";
-        //echo "Inserted";
-        mysqli_query($db, $sql);
-
-        $_SESSION['UserName'] = $name;
-        $_SESSION['success'] = "You are now signed Up";
-
-        //header('location: index.php');
-        mysqli_close($db);
-    }
-
-    $db->close();
 ?>
 <?php if (is_countable($errors) && count($errors) > 0) : ?>
     <div>
-    <?php foreach ($errors as $error) : ?>
+    <?php foreach($errors as $error) : ?>
     <p><?php echo $error ?></p>
     <?php endforeach ?>
     <!--redirect to index.php after 2 seconds-->
-<?php //header("Refresh:2; url= index.php"); ?>
-</div>
-
-<?php endif ?>
+    <?php header("Refresh:2; url= index.php"); ?>
+    </div>
+    
+    <?php endif ?>
+     
