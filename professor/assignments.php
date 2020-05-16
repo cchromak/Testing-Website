6f8ff424f0f42ce115b2b9e6d1ab919f5127e734
kevcardenas99 <kevcardenas99@gmail.com>
2020-05-16 02:27:35 -0400

    changed some html files to php

---
+++ professor/assignments.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="styleIndex.css">
+  <title>Learning Is Fun</title>
+</head>
+<body>
+	<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="p-4" style="background-color: aliceblue">
+        <h4 class="text-black"><a href="professor.html" class="no-underline">Professor Dashboard</a></h4>
+        <h4 class="text-black"><a href="pStudents.html" class="no-underline">Students</a></h4>
+        <h4 class="text-black"><a href="assignments.html" class="no-underline">Assignments</a></h4>
+        <h4 class="text-black"><a href="aboutus.php" class="no-underline">About Us</a></h4>
+        <h4 class="text-black"><a href="contactus.html" class="no-underline">Contact Us</a></h4>
+        <h4 class="text-black"><a href="help.html" class="no-underline">Help</a></h4>
+        <h4 class="text-black"><a href="../index.php" class="no-underline">Log out</a></h4>
+      </div>
+    </div>
+    <nav class="navbar navbar-light" style="background-color: aliceblue">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  </div>
+
+  <div class="container">
+    <h1 class="display-1">Here Is Your Professor Dashboard</h1>
+  </div>
+  <div class ="container">
+  </div>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
