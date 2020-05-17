1e6a858f9bb67b89e181b86e97ec9ee764ef9b52
kevcardenas99 <kevcardenas99@gmail.com>
2020-05-17 15:20:02 -0400

    Added a page for assignment questions to go for students

--- student/answerQ.php
+++ student/answerQ.php
@@ -50,22 +50,7 @@
     </nav>
   </div>
   <div class="dashboardLabel">
-    <span>Here Are Your Assignments</span>
-  </div>
-  <div>
-    <?php
-      $db = mysqli_connect('mars.cs.qc.cuny.edu', 'cake2827', '23682827', 'cake2827') or die("could not connect to database");  //select question_sets from database
-      $sql = "SELECT * from questionset";
-      $results = mysqli_query($db, $sql);
-      while($row = $results->fetch_row()){
-        //create a div to hold each assignment
-        echo '<div class="aList">';
-        echo sprintf("<a href=answerQ.php?set=%s>", $row[0]);
-        echo sprintf("%s </a>", $row[1]);
-        echo '</div>';
-      }
-      mysqli_close($db);
-    ?>
+    <span>Here Are the questions for this Assignments</span>
   </div>
   <div class ="container">
   </div>
