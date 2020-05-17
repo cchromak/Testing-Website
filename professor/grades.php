d3c2a8020421a8f9b7b1bb157045e1fc95aeceaa
Charly <cgomez9001@gmail.com>
2020-05-17 03:18:36 -0400

    FIXED SOME GOOD STUFFS

--- professor/grades.php
+++ professor/grades.php
@@ -4,15 +4,33 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="basicStyle.css">
+  <link rel="stylesheet" href="styleIndex.css">
   <title>Teacher's Lounge</title>
 </head>
 <body>
-  <div class="container">
+	<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="p-4" style="background-color: aliceblue">
+        <h4 class="text-black"><a href="professor.php" class="no-underline">Professor Dashboard</a></h4>
+        <h4 class="text-black"><a href="makeQuestions.php" class="no-underline">Create Questions</a></h4>
+        <h4 class="text-black"><a href="assignments.php" class="no-underline">Assignments</a></h4>
+        <h4 class="text-black"><a href="aboutus.php" class="no-underline">About Us</a></h4>
+        <h4 class="text-black"><a href="contactus.php" class="no-underline">Contact Us</a></h4>
+        <h4 class="text-black"><a href="help.php" class="no-underline">Help</a></h4>
+        <h4 class="text-black"><a href="../logout.php" class="no-underline">Log out</a></h4>
+      </div>
+    </div>
+    <nav class="navbar navbar-light" style="background-color: aliceblue">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav> 
+  </div>
+  <div class="container" style="background-color: aliceblue">
     <h1 index="greeting" class="display-1 h1">Grading Notebook</h1>
   </div>
-  <table class="table">
-    <thead class="thead-dark">
+  <table class="table" style="background-color: aliceblue"> 		
+    <thead class="thead-dark" style="background-color: aliceblue">
       <tr>
         <th scope="col">#</th>
         <th scope="col">First Name</th>
@@ -20,7 +38,7 @@
         <th scope="col">Grades</th>
       </tr> 
     </thead>
-    <tbody> 
+    <tbody style="background-color: aliceblue"> 
       <?php 
         $db = mysqli_connect('mars.cs.qc.cuny.edu', 'cake2827', '23682827', 'cake2827') or die("could not connect to database");
         $sql_grades_receiver = "SELECT grade FROM grades";
@@ -28,17 +46,18 @@
         $row = $results->fetch_row();
         echo "<tr>
         <td> 1 </td>  
+        <td> Charly </td>
+        <td> Gomez </td>  
+        <td>" . $row[0] . "</td>
+        </tr>";
+        $row = $results->fetch_row(); 
+        echo "<tr>
+        <td> 2 </td>  
         <td> Kevin </td>
         <td> Cardenas </td> 
         <td>" . $row[0] . "</td>
-        </tr>";
+        </tr>"; 
         $row = $results->fetch_row();
-        echo "<tr>
-        <td> 2 </td>  
-        <td> Charly </td>
-        <td> Gomez </td> 
-        <td>" . $row[0] . "</td>
-        </tr>";$row = $results->fetch_row();
         echo "<tr>
         <td> 3 </td>  
         <td> Chris </td>
@@ -47,7 +66,8 @@
         </tr>";
       ?>
     </tbody>
-  </table>  
+     
+  </table> 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
