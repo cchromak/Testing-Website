17ecf08a589ac11b0bd3ec7d320b5ca9c2173d1c
kevcardenas99 <kevcardenas99@gmail.com>
2020-05-18 13:55:03 -0400

    more fixes

--- student/contactus.php
+++ student/contactus.php
@@ -35,8 +35,7 @@
   <div class="pos-f-t">
     <div class="collapse" id="navbarToggleExternalContent">
       <div class="p-4" style="background-color: aliceblue">
-        <h4 class="text-black"><a href="professor.php" class="no-underline">Professor Dashboard</a></h4>
-        <h4 class="text-black"><a href="makeQuestions.php" class="no-underline">Create Questions</a></h4>
+        <h4 class="text-black"><a href="student.php" class="no-underline">Student Dashboard</a></h4>
         <h4 class="text-black"><a href="assignments.php" class="no-underline">Assignments</a></h4>
         <h4 class="text-black"><a href="grades.php" class="no-underline">Grades</a></h4>
         <h4 class="text-black"><a href="aboutus.php" class="no-underline">About Us</a></h4>
