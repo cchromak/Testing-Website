7fee8e735593ad0bc5ee44c5848247819bf3fa99
kevcardenas99 <kevcardenas99@gmail.com>
2020-05-18 13:38:10 -0400

    Added Chris's changes to all contact pages

--- professor/contactus.php
+++ professor/contactus.php
@@ -31,17 +31,15 @@
   <title>Learning Is Fun</title>
 </head>
 <body>
+  <!-- This div holds the navbar -->
   <div class="pos-f-t">
     <div class="collapse" id="navbarToggleExternalContent">
       <div class="p-4" style="background-color: aliceblue">
-        <h4 class="text-black"><a href="professor.php" class="no-underline">Professor Dashboard</a></h4>
-        <h4 class="text-black"><a href="makeQuestions.php" class="no-underline">Create Questions</a></h4>
-        <h4 class="text-black"><a href="assignments.php" class="no-underline">Assignments</a></h4>
-        <h4 class="text-black"><a href="grades.php" class="no-underline">Grades</a></h4>
+        <h4 class="text-black"><a href="index.php" class="no-underline">Log in</a></h4>
+        <h4 class="text-black"><a href="register.php" class="no-underline">Register</a></h4>
         <h4 class="text-black"><a href="aboutus.php" class="no-underline">About Us</a></h4>
-        <h4 class="text-black"><a href="contactus.php" class="no-underline">Contact Us</a></h4>
-        <h4 class="text-black"><a href="help.php" class="no-underline">Help</a></h4>
-        <h4 class="text-black"><a href="../logout.php" class="no-underline">Log out</a></h4>
+        <h4 class="text-black"><a href="contactus.html" class="no-underline">Contact Us</a></h4>
+        <h4 class="text-black"><a href="help.html" class="no-underline">Help</a></h4>
       </div> 
     </div>
     <nav class="navbar navbar-light" style="background-color: aliceblue">
@@ -50,22 +48,26 @@
       </button>
     </nav>
   </div>
+
+  <!-- This holds the message on our page -->
   <div class="container">
     <h1 index="greeting" class="display-1">Send us an Email!</h1>
   </div>
+
+  <!-- This holds the form to send an email -->
   <main>
     <form class="contact-form" action="contact.php" method="post">
       <div class="form-group">
-        <input type="text" name="name" placeholder="Enter Name">
+        <input type="text" class="form-control" name="name" placeholder="Enter Name">
       </div>
       <div class="form-group">
-        <input type="text" name="email" placeholder="Enter Email">
+        <input type="text" class="form-control" name="email" placeholder="Enter Email">
       </div>
       <div class="form-group">
-        <input type="text" name="subject" placeholder="Enter Subject">
+        <input type="text" class="form-control" name="subject" placeholder="Enter Subject">
       </div>
       <div class="form-group"></div>
-        <textarea name="message" placeholder="What do you want to tell us?"></textarea>
+        <textarea name="message" class="form-control" placeholder="What do you want to tell us?"></textarea>
       </div>
       <div class="container">
       	<br>
@@ -75,10 +77,11 @@
   </main>
 
   <div class="container">
-  	  <br><br>
-      <a class="twitter-timeline" data-width="600" data-height="400" data-theme="light" href="https://twitter.com/EpicsauceT?ref_src=twsrc%5Etfw">Tweets by EpicsauceT</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-  </div>
+    <br><br>
+    <a class="twitter-timeline" data-width="600" data-height="400" data-theme="light" href="https://twitter.com/EpicsauceT?ref_src=twsrc%5Etfw">Tweets by EpicsauceT</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
 
+  <!-- Javascript -->
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
