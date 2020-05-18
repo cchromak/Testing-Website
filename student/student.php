f18d53618d121c4357732006cdd394a55357fe9f
cchromak <cchromak@gmail.com>
2020-05-18 13:40:13 -0400

    Added links to studend dashboard

--- student/student.php
+++ student/student.php
@@ -49,11 +49,46 @@
       </button>
     </nav>
   </div>
-  <div class="dashboardLabel">
-    <span>Here Is Your Student Dashboard</span>
+
+  <div class="container">
+    <h1 class="display-1">Student Dashboard</h1>
   </div>
+  
   <div class ="container">
+	  <div class="alert alert-primary alert-dismissible fade show" role="alert">
+	  <strong>Hint:</strong> Getting a good nights sleep the night before a test is important. :) 
+	  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+	    <span aria-hidden="true">&times;</span>
+	  </button>
   </div>
+	
+  <div class="container mt-4">
+    <div class="row d-flex justify-content-center">
+        
+        <div class="col-auto mb-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Grades</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">The grading notebook</h6>
+                    <p class="card-text">The student can check graded assignments from their virtual notebook.</p>
+                    <a href="https://venus.cs.qc.cuny.edu/~cake2827/epic/student/grades.php" class="card-link">Grades Page</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-auto mb-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Assignments</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Take an assignments</h6>
+                    <p class="card-text">The student can take multiple assingments given to them by their professor.</p>
+                    <a href="https://venus.cs.qc.cuny.edu/~cake2827/epic/student/assignments.php" class="card-link">Create Assignment</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  
+
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
